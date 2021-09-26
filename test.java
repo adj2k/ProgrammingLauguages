eb37118bddbd4495d372340f52d530801aa13d10
adj2k <69940641+adj2k@users.noreply.github.com>
2021-09-26 16:50:31 -0500

    Add files via upload

---
+++ test.java
@@ -0,0 +1,50 @@
+// Java program to Get the size of a directory
+ 
+import java.io.File;
+
+
+class GFG {
+    private static long getFolderSize(File folder)
+    {
+        long length = 0;
+       
+        // listFiles() is used to list the
+        // contents of the given folder
+        File[] files = folder.listFiles();
+ 
+        long count = files.length;
+ 
+        // loop for traversing the directory
+        for (int i = 0; i < count; i++) {
+            if (files[i].isFile()) {
+                length += files[i].length();
+            }
+            else {
+                length += getFolderSize(files[i]);
+            }
+        }
+        return length;
+    }
+ 
+    // Driver Code
+    public static void main(String[] args)
+    {
+        // Creating an instances of file class
+        File file1 = new File("/home/pi/Documents");
+   
+   
+        
+ 
+        long size = getFolderSize(file1);
+        // Size of folder in Bytes
+        System.out.println("Size of " + file1 + " is "
+                           + size + " B");
+        // Size of folder in Kilobytes
+        System.out.println("Size of " + file1 + " is "
+                           + (double)size / 1024 + " KB");
+        // Size of folder in Megabytes
+        System.out.println("Size of " + file1 + " is "
+                           + (double)size / (1024 * 1024)
+                           + " MB");
+    }
+}
